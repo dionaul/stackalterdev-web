4a5b5940c2c3fcd2350d84b4e7c99946f911a8bd
rukandafaridsi <rukanda@intimap.com>
2017-11-12 14:14:53 +0700

    Sample Code

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+
+const Dashboard = () => import('@/page/Dashboard')
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'Dashboard',
+      component: Dashboard
     }
   ]
 })
